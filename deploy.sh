813b86b8d3ffd91b53505ec3d2fce8bf90752b4b
Ted Alff <twa022@gmail.com>
2020-04-14 00:40:51 -0400

    Change deploy script to delete and recreate the gh-pages branch.

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
+git branch -d gh-pages
+git checkout -b gh-pages || exit 1
 
-# Minify the javascript and CSS files
+# Minify the javascript file
 sed -e '/console.log/d' main.js | minify --mime 'text/javascript' -o main.js
 
 find . -mindepth 1 -maxdepth 2 -type f -name "*.css" -not -name "*.min.css" -exec minify '{}' -o '{}' \;
 find . -mindepth 1 -maxdepth 2 -type f -name "*.json" -not -name "*.min.json" -exec minify '{}' -o '{}' \;
 
-git stash
-git branch gh-pages
-git checkout gh-pages || exit 1
-git stash pop
-
 git rm deploy.sh
 git rm reset.sh
+
+git push -f
 
 exit 0
